51f0ec30a511cb4d8e4dc3966b646f96b81d1f1f
Dinh Nguyen Anh Dung <dungdna2000@gmail.com>
2021-08-02 20:09:37 +0700

    Fix minor SAL warning on WinMain header

--- 00-Intro/main.cpp
+++ 00-Intro/main.cpp
@@ -481,7 +481,7 @@ void Cleanup()
 
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance,
-	_In_ HINSTANCE hPrevInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine,
 	_In_ int nCmdShow
 )
