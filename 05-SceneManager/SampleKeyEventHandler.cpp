dd2590fb2d02ea0cc6e0854f250460eec8ff3724
Dinh Nguyen Anh Dung <dungdna2000@gmail.com>
2021-10-11 19:57:36 +0700

    Done moving Marrio asset to file. Add hot key 0 to kill Mario quickly

--- 05-SceneManager/SampleKeyEventHandler.cpp
+++ 05-SceneManager/SampleKeyEventHandler.cpp
@@ -25,6 +25,9 @@ void CSampleKeyHandler::OnKeyDown(int KeyCode)
 	case DIK_2:
 		mario->SetLevel(MARIO_LEVEL_BIG);
 		break;
+	case DIK_0:
+		mario->SetState(MARIO_STATE_DIE);
+		break;
 	case DIK_R: // reset
 		//Reload();
 		break;
