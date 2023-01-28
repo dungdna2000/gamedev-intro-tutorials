d8ae8fa9048d58c47202fe6f4820978d7bfbd2cf
Dinh Nguyen Anh Dung <dungdna2000@gmail.com>
2023-01-28 21:04:38 +0700

    Fix x64 crash & update to VS2022 in 05-SceneManager

--- 05-SceneManager/Game.cpp
+++ 05-SceneManager/Game.cpp
@@ -48,7 +48,7 @@ void CGame::Init(HWND hWnd, HINSTANCE hInstance)
 
 	// Create the D3D device and the swap chain
 	HRESULT hr = D3D10CreateDeviceAndSwapChain(NULL,
-		D3D10_DRIVER_TYPE_REFERENCE,
+		D3D10_DRIVER_TYPE_HARDWARE,
 		NULL,
 		0,
 		D3D10_SDK_VERSION,
